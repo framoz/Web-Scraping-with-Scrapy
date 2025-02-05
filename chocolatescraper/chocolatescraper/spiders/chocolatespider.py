2fda80eafb6ca6be1caf8b49b8ee532feb031b91
framoz <felipe.bueno.ramos@gmail.com>
2025-02-05 19:49:24 +0000

    Done first parse on Chocolatespider

--- chocolatescraper/chocolatescraper/spiders/chocolatespider.py
+++ chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -7,4 +7,14 @@ class ChocolatespiderSpider(scrapy.Spider):
     start_urls = ["https://chocolate.co.uk/collections/all"]
 
     def parse(self, response):
-        pass
+
+        products = response.css('product-item')
+
+        for product in products:
+                        
+            yield{
+                'name' : product.css('a.product-item-meta__title::text').get(),
+                'price' : product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>£', '').replace('</span>'),
+                'url' : product.css('a.product-item-meta__title').attrib['href']
+            }
+        
